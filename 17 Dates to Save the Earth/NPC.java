86b078579f5ed9b7c57702bf37b14e0b942e8ae0
edik96 <edik96@mail.ru>
2025-03-06 20:18:15 +0100

    reformat NPC class

--- 17 Dates to Save the Earth/NPC.java
+++ 17 Dates to Save the Earth/NPC.java
@@ -1,35 +1,39 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class NPC here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class NPC extends Actor
-{
-    public NPC(String name, int affection){
-    _name = name;
-    _affection = affection;
+public class NPC extends Actor {
+
+    public NPC(String name, int affection) {
+        _name = name;
+        _affection = affection;
     }
     private int _affection = 0;
-    public int getaffection(){
-    return _affection;
+
+    public int getaffection() {
+        return _affection;
     }
-    public void setaffection(int value){
-    _affection = value;
-    
+
+    public void setaffection(int value) {
+        _affection = value;
+
     }
     private String _name = "";
-    public String getName(){
-    return _name;
+
+    public String getName() {
+        return _name;
     }
+
     /**
-     * Act - do whatever the NPC wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - do whatever the NPC wants to do. This method is called whenever the
+     * 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        
+    public void act() {
+
     }
 }
