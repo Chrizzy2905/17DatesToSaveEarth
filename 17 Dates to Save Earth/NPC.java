591a76df4053128e1679e4a08516201f55e54a6b
Chrizzy2905 <christopherlenz29@gmail.com>
2025-03-07 19:17:49 +0100

    addObject

--- 17 Dates to Save Earth/NPC.java
+++ 17 Dates to Save Earth/NPC.java
@@ -8,28 +8,36 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class NPC extends Actor {
-
+    private GreenfootImage _image;
     public NPC(String name, int affection) {
+        super();
+        _image = new GreenfootImage(name + ".png");
+        setImage(_image);
         _name = name;
         _affection = affection;
+        
     }
     
     private int _affection = 0;
 
-    public int getaffection() {
+    public int getAffection() {
         return _affection;
     }
 
-    public void setaffection(int value) {
+    public void setAffection(int value) {
         _affection = value;
     }
-
+    
+    public void addAffection(int value){
+        _affection += value;
+    }
+    
     private String _name = "";
 
     public String getName() {
         return _name;
     }
-
+    
     /**
      * Act - do whatever the NPC wants to do. This method is called whenever the
      * 'Act' or 'Run' button gets pressed in the environment.
