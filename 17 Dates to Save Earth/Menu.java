0b9936e9de4045df1cfa3a4b521fae292d02b6b4
edik96 <edik96@mail.ru>
2025-03-07 21:08:44 +0100

    add main menu add dialog add menubutton add scene switch

---
+++ 17 Dates to Save Earth/Menu.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Dialog here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu extends World
+{
+
+    /**
+     * Constructor for objects of class Dialog.
+     * 
+     */
+    public Menu()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        GreenfootImage bg = new GreenfootImage("SplashScreen.jpg");
+        bg.scale(600, 400);
+        setBackground(bg);
+        
+        // Create Menu
+        MenuButton newGame = new MenuButton("New Game", Action.NEWGAME);
+        addObject(newGame, 100, 200);
+        MenuButton loadGame = new MenuButton("Load Game", Action.LOAD);
+        addObject(loadGame, 100, 240);
+        MenuButton exitGame = new MenuButton("Exit", Action.EXIT);
+        addObject(exitGame, 100, 280);
+    }
+}
